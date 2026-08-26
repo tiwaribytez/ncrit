50bad32726b084305a3697a5fe81ba6af0342571
tiwaribytez <anand7k07@gmail.com>
2026-08-26 09:59:25 +0000

    last

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,12 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+
   return [
     {
       url: baseUrl,
